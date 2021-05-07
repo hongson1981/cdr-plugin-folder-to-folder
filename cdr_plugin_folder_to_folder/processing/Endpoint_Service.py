050719c17834134d609a6ee18bedaf18681cf38e
Harut Grigoryan <harut70@hotmail.com>
2021-05-07 12:28:28 +0400

    Ebdpoit_Service stub

---
+++ cdr_plugin_folder_to_folder/processing/Endpoint_Service.py
@@ -0,0 +1,65 @@
+import threading
+import logging as logger
+from time import sleep
+
+from cdr_plugin_folder_to_folder.storage.Storage    import Storage
+from cdr_plugin_folder_to_folder.utils.Log_Duration import log_duration
+
+logger.basicConfig(level=logger.INFO)
+
+class Endpoint_Service:
+
+    lock = threading.Lock()
+
+    _instance = None
+    def __new__(cls):                                               # singleton pattern
+        if cls._instance is None:
+            cls._instance = super(Endpoint_Service, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if hasattr(self, 'instantiated') is False:                     # only set these values first time around
+            self.instantiated   = True
+            self.storage        = Storage()
+            self.service_thread_on = False
+            self.service_thread = threading.Thread()
+
+    @classmethod
+    def clear_instance(cls):
+        del cls.instance
+
+    def ServiceThread(self, update_interval):
+        while self.service_thread_on:
+            self.update_endpoints_list()
+            sleep(update_interval)
+
+    def StartServiceThread(self):
+        if self.service_thread_on:
+            return
+
+        self.service_thread_on = True
+        self.service_thread = threading.Thread(target=self.ServiceThread, args=(1,))
+        self.service_thread.start()
+
+    def StopServiceThread(self):
+        self.service_thread_on = False
+        self.service_thread.join()
+
+    def load_data(self):
+        return self
+
+    def reset(self):
+        return self
+
+    def save(self):
+        return self
+
+    def update_endpoints_list(self):
+        Endpoint_Service.lock.acquire()
+        try:
+            pass
+        finally:
+            Endpoint_Service.lock.release()
+            self.save()
+
+        return self
