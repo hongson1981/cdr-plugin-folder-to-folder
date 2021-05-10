5b41815cea26af3e2e23bb5b726f7d312622f85a
Harut Grigoryan <harut70@hotmail.com>
2021-05-10 12:48:20 +0400

    Update test_Configure_Env

--- tests/integration/configure/test_Configure_Env.py
+++ tests/integration/configure/test_Configure_Env.py
@@ -1,25 +1,29 @@
 from unittest import TestCase
 
 from osbot_utils.utils.Dev import pprint
-from osbot_utils.utils.Json import json_to_str, json_parse
+from osbot_utils.utils.Json import str_to_json, json_to_str, json_parse
+from osbot_utils.utils.Http import GET_json, GET
 
 from cdr_plugin_folder_to_folder.configure.Configure_Env import Configure_Env
-
 
 class test_Configure_Env(TestCase):
 
     def setUp(self):
         self.configure_env = Configure_Env()
 
-    def test_get_valid_endpoints(self):
 
-        test_ips =  [
-                        "3.249.16.227"
-                    ]
+    def test_get_valid_endpoints(self):
+        url = 'https://tmol8zkg3c.execute-api.eu-west-1.amazonaws.com/prod/sdk-servers/ip_addresses'
+        ips = []
+        try:
+            ips = str(GET_json(url).get('live_ips'))
+        except:
+            assert False
+        ips =  ips.replace("'", '"')
 
         endpoints = []
-        for ip in test_ips:
-            endpoints.append({'IP': ip, "Port": "8080"})
+        for ip in str_to_json(ips):
+            endpoints.append({'IP': ip , "Port": "8080"})
 
         valid_endpoints = {'Endpoints' : endpoints }
         endpoint_string = json_to_str(valid_endpoints)
