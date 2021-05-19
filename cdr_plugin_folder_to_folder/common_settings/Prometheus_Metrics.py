4efd640167f052d1c134a5604bef6cfb7aec0d8a
Harut Grigoryan <harut70@hotmail.com>
2021-05-19 11:34:02 +0400

    get_metric_from_text method

--- cdr_plugin_folder_to_folder/common_settings/Prometheus_Metrics.py
+++ cdr_plugin_folder_to_folder/common_settings/Prometheus_Metrics.py
@@ -4,17 +4,21 @@ import psutil
 import logging as logger
 from time import sleep
 
-from prometheus_client import start_http_server
+from prometheus_client import start_http_server, Gauge
 
-from osbot_utils.utils.Files                        import path_combine, folder_create, file_create
-from osbot_utils.utils.Json                         import json_save_file_pretty, json_load_file, file_exists
-from cdr_plugin_folder_to_folder.storage.Storage    import Storage
-from cdr_plugin_folder_to_folder.utils.Log_Duration import log_duration
-from cdr_plugin_folder_to_folder.utils.PS_Utils import PS_Utils
+# from osbot_utils.utils.Files                        import path_combine, folder_create, file_create
+# from osbot_utils.utils.Json                         import json_save_file_pretty, json_load_file, file_exists
+# from cdr_plugin_folder_to_folder.storage.Storage    import Storage
+# from cdr_plugin_folder_to_folder.utils.Log_Duration import log_duration
+# from cdr_plugin_folder_to_folder.utils.PS_Utils import PS_Utils
 
 from cdr_plugin_folder_to_folder.common_settings.Config import Config
 
 logger.basicConfig(level=logger.INFO)
+
+class MetricNames:
+    HD1_FILES_COUNT = 'hd1_files_count'
+
 
 class Prometheus_Metrics:
 
@@ -29,9 +33,22 @@ class Prometheus_Metrics:
             self.instantiated   = True
             self.config = Config()
             start_http_server(self.config.prometheus_port)
+            self.status_files_count = Gauge(MetricNames.HD1_FILES_COUNT,'Total number of files on HD1')
+            self.set_status_files_count(0)
 
     @classmethod
     def clear_instance(cls):
         del cls.instance
+
+    def get_metric_from_text(self, text, metric_name):
+        metric_name_position = text.find(f'\n{metric_name}') + 1
+        if metric_name_position < 0: # not found
+            return None
+        metric_position = metric_name_position + len(metric_name)
+        end_of_line_position = text.find('\n', metric_position)
+        return text[metric_position:end_of_line_position].strip()
+
+    def set_status_files_count(self, count):
+        self.status_files_count.set(count)
 
 
--- tests/unit/common_settings/test_Prometheus_Metrics.py
+++ tests/unit/common_settings/test_Prometheus_Metrics.py
@@ -1,24 +1,32 @@
 from os import environ
 from unittest import TestCase
 from osbot_utils.utils.Dev import pprint
-from osbot_utils.utils.Misc import list_set
+from osbot_utils.utils.Misc import is_number, list_set
 
 from cdr_plugin_folder_to_folder.common_settings.Config import Config
-from cdr_plugin_folder_to_folder.common_settings.Prometheus_Metrics import Prometheus_Metrics
+from cdr_plugin_folder_to_folder.common_settings.Prometheus_Metrics import Prometheus_Metrics, MetricNames
 from cdr_plugin_folder_to_folder.utils.testing.Setup_Testing import Setup_Testing
 from cdr_plugin_folder_to_folder.utils.testing.Test_Data import Test_Data
 
 import requests
-from prometheus_client import start_http_server
 
 class test_Prometheus_Metrics(TestCase):
 
     def setUp(self) -> None:
         self.config = Config()
         self.metrics = Prometheus_Metrics()
-        pass
+
+    def is_number(self, s):
+        try:
+            float(s)
+            return True
+        except ValueError:
+            return False
 
     def test_prometheus_data(self):
         prometheus_url = f'http://{self.config.prometheus_host}:{self.config.prometheus_port}'
         prometheus_data = requests.get(prometheus_url).text
         assert prometheus_data
+        hd1_files_count = self.metrics.get_metric_from_text(prometheus_data, MetricNames.HD1_FILES_COUNT)
+        assert hd1_files_count
+        assert self.is_number(hd1_files_count)
