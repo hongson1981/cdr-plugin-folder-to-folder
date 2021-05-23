e3e0aabcd6794469083463555e9bbe80855e6517
Dinis Cruz <dinis.cruz@owasp.org>
2021-05-23 18:51:38 +0100

    adding npm install to dockerfile couple other minor fixes

--- bin/website-docker-build-run.sh
+++ bin/website-docker-build-run.sh
@@ -1,7 +1,7 @@
 cd website
-docker build -t cdr_plugin_folder_to_folder_docs .
+docker build -t cdr_plugin_folder_to_folder_website .
 docker  run --rm -it -p 1313:1313            \
         -v $(pwd)/site:/src/site             \
         -v $(pwd)/content:/src/site/content  \
         -v $(pwd)/data:/src/site/data        \
-        cdr_plugin_folder_to_folder_docs
+        cdr_plugin_folder_to_folder_website
