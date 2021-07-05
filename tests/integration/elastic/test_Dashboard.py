d7580408445930318c036c67451d023bfd2dbf18
Harut Grigoryan <harut70@hotmail.com>
2021-07-05 12:38:08 +0400

    Refactor test_Dashboard test

--- tests/integration/elastic/test_Dashboard.py
+++ tests/integration/elastic/test_Dashboard.py
@@ -43,27 +43,7 @@ class test_Dashboard(TestCase):
         assert Index_Pattern(kibana=self.kibana, pattern_name=random_text()).info() == {}
         assert self.index_pattern.delete() is True
 
-    def test_delete(self):
-        assert self.index_pattern.delete() is False
-
-    def test_id(self):
-        assert self.index_pattern.id() is None
-
-    def test_info(self):
-        assert self.index_pattern.info() == {}
-
     def test_import_dashboard(self):
-        # dashboard_file = 'processed-files-v8.ndjson'
-        # dashboard_file = 'KD1.ndjson'
-        # url_dashboards = 'https://raw.githubusercontent.com/filetrust/cdr-plugin-folder-to-folder-test-data/main/kibana-dashboards/'
-        # url_dashboard = url_dashboards + dashboard_file
-        # dashboard_data = GET(url_dashboard)
-        #
-        # import_file = file_create(extension=dashboard_file, contents=dashboard_data)
-        # dashboard = Dashboard(kibana=self.kibana)
-        # #import_file = '/var/folders/_j/frqs70d93l328f307rw2jx5h0000gn/T/tmp6gz6sc76.tmp'
-        # #import_data = file_contents(export_file)
-        # pprint(self.dashboard.import_dashboard(import_file= import_file))
         dashboard = Dashboard(kibana=self.kibana)
         pprint(dashboard.import_dashboard_from_github(dashboard_file_name='processed-files-v8.ndjson'))
         pprint(dashboard.import_dashboard_from_github(dashboard_file_name='KD1.ndjson'))
