a2eec3e65f03f1a1e1c643683dd64439e684daf0
Harut Grigoryan <harut70@hotmail.com>
2021-07-05 17:36:40 +0400

    Introduce Dashboard_Manager class

---
+++ cdr_plugin_folder_to_folder/utils/_to_refactor/For_OSBot_Elastic/Dashboard_Manager.py
@@ -0,0 +1,44 @@
+from osbot_utils.utils.Files import file_create, path_combine
+from osbot_utils.utils.Http import GET
+
+import os
+from cdr_plugin_folder_to_folder.utils._to_refactor.For_OSBot_Elastic.Dashboard import Dashboard
+
+class Dashboard_Manager:
+
+    def __init__(self, kibana):
+        self.kibana         = kibana
+        self.object_type    = 'dashboard_manager'
+        pass
+
+    def isDashbooard(self, filepath):
+        if not os.path.exists(filepath):
+            return False
+        if not os.path.isfile(filepath):
+            return False
+        filename = os.path.basename(filepath)
+        if not filename.endswith(".ndjson"):
+            return False
+        # TODO: more verifiations on the file content
+        return True
+
+    def import_dashboards(self, directory):
+        if not os.path.exists(directory):
+            return -1
+
+        if not os.path.isdir(directory):
+            return -1
+
+        count = 0
+        for key in os.listdir(directory):
+            filepath = path_combine(directory, key)
+            if not self.isDashbooard(filepath):
+                continue
+            try:
+                dashboard = Dashboard(kibana=self.kibana)
+                dashboard.import_dashboard(filepath)
+                count += 1
+            except:
+                pass
+
+        return count
