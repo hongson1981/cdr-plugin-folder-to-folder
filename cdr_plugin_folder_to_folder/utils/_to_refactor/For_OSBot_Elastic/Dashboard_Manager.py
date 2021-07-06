553b945eddfe451ac340f05bbb1b89c4a85d57f9
Harut Grigoryan <harut70@hotmail.com>
2021-07-06 10:45:24 +0400

    Wait for Kibana to come up with uploading the dashboards

--- cdr_plugin_folder_to_folder/utils/_to_refactor/For_OSBot_Elastic/Dashboard_Manager.py
+++ cdr_plugin_folder_to_folder/utils/_to_refactor/For_OSBot_Elastic/Dashboard_Manager.py
@@ -2,7 +2,11 @@ from osbot_utils.utils.Files import file_create, path_combine
 from osbot_utils.utils.Http import GET
 
 import os
+import time
 from cdr_plugin_folder_to_folder.utils._to_refactor.For_OSBot_Elastic.Dashboard import Dashboard
+from cdr_plugin_folder_to_folder.utils._to_refactor.For_OSBot_Elastic.Kibana import Kibana
+
+KIBANA_TIMEOUT = 10
 
 class Dashboard_Manager:
 
@@ -30,6 +34,14 @@ class Dashboard_Manager:
             return -1
 
         count = 0
+
+        for i in range(0, KIBANA_TIMEOUT):
+            if i == KIBANA_TIMEOUT:
+                return count
+            if self.kibana.enabled:
+                break
+            time.sleep(6) # 6 seconds delay
+
         for key in os.listdir(directory):
             filepath = path_combine(directory, key)
             if not self.isDashbooard(filepath):
