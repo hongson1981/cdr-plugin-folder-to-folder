b88709dd3c5d207d68f570d44d5bad9cf2dd5d41
Harut Grigoryan <harut70@hotmail.com>
2021-07-13 16:35:29 +0400

    Eliminate utilization of file_service.py

--- tests/unit/utils/test_File_utils.py
+++ tests/unit/utils/test_File_utils.py
@@ -5,10 +5,10 @@ from unittest import TestCase
 from osbot_utils.utils.Files import folder_exists, temp_folder, file_exists, folder_temp, folder_delete_all, temp_file, \
     file_copy, create_folder
 
-from cdr_plugin_folder_to_folder.pre_processing.utils.file_service import File_Service
+from cdr_plugin_folder_to_folder.utils.file_utils import FileService
 from cdr_plugin_folder_to_folder.utils.testing.Test_Data import Test_Data
 
-class test_File_service(TestCase):
+class test_File_utils(TestCase):
     #test_folder="./test_data/test_files"
     #new_folder=os.path.join(test_folder, "sample")
     new_folder = None
@@ -24,22 +24,13 @@ class test_File_service(TestCase):
 
     def setUp(self) -> None:
         self.test_data    = Test_Data()
-        self.file_service = File_Service()
+        self.file_service = FileService()
         self.test_folder  = self.test_data.path_test_files
         self.test_file    = self.test_data.image()
         self.dict_content = { "value": "testing" }
 
-    def test_wrtie_json_file(self):
-        create_folder(self.new_folder)
-        self.file_service.wrtie_json_file(self.new_folder,"test.json",self.dict_content)
-
-        assert os.path.exists(os.path.join(self.new_folder,"test.json")) is True
-
-    def test_read_json_file(self):
-        json_file_path=self.test_data.json()
-        content=self.file_service.read_json_file(json_file_path)
-
-        assert content is not None
+    def test_files_in_folder(self):
+        pass
 
 
 
