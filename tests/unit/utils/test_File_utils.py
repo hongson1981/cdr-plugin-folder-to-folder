9d126a775d8d6c32e5038b7c46b70f26d8f05c74
Harut Grigoryan <harut70@hotmail.com>
2021-07-13 17:07:59 +0400

    file_utils tests

--- tests/unit/utils/test_File_utils.py
+++ tests/unit/utils/test_File_utils.py
@@ -2,11 +2,15 @@ import os
 import shutil
 from unittest import TestCase
 
-from osbot_utils.utils.Files import folder_exists, temp_folder, file_exists, folder_temp, folder_delete_all, temp_file, \
-    file_copy, create_folder
+from osbot_utils.utils.Files import temp_folder, path_combine
 
 from cdr_plugin_folder_to_folder.utils.file_utils import FileService
 from cdr_plugin_folder_to_folder.utils.testing.Test_Data import Test_Data
+
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'test_files',
+    )
 
 class test_File_utils(TestCase):
     #test_folder="./test_data/test_files"
@@ -30,9 +34,24 @@ class test_File_utils(TestCase):
         self.dict_content = { "value": "testing" }
 
     def test_files_in_folder(self):
-        pass
+        assert 3 == len(self.file_service.files_in_folder(FIXTURE_DIR))
 
+    def test_base64encode(self):
+        assert "cGxlYXN1cmUuCg==" == self.file_service.base64encode(path_combine(FIXTURE_DIR, "pleasure"))
+        assert "bGVhc3VyZS4K" == self.file_service.base64encode(path_combine(FIXTURE_DIR, "leasure"))
+        assert "ZWFzdXJlLgo=" == self.file_service.base64encode(path_combine(FIXTURE_DIR, "easure"))
 
+    def test_base64decode(self):
+        decoded = self.file_service.base64decode("cGxlYXN1cmUu")
+        assert decoded == b"pleasure."
+        decoded = self.file_service.base64decode("bGVhc3VyZS4=")
+        assert decoded == b"leasure."
+        decoded = self.file_service.base64decode("ZWFzdXJlLg==")
+        assert decoded == b"easure."
+        decoded = self.file_service.base64decode("YXN1cmUu")
+        assert decoded == b"asure."
+        decoded = self.file_service.base64decode("c3VyZS4=")
+        assert decoded == b"sure."
 
 
 
